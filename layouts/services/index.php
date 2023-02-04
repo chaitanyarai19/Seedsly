0bea6253011a16826ad69798a8005439f2e9de59
chaitanyarai19 <chaitanyarai19@gmail.com>
2023-02-05 00:23:40 +0530

    seedsly_fol7

---
+++ layouts/services/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+
+<?php include("navbar.php"); ?>
+
+<section>
+<div id="icon-wrapper">
+  <a href="#">
+    <div class="icons">
+      <div class="icon-slide-container">
+        <img class="slide-icon"  alt="" height="100" src="alert.jpg">
+        </div>
+      </div>
+    </a>
+    
+    <a href="kite-map.php">
+      <div class="icons2">
+        <div class="icon-slide-container">
+          <img class="slide-icon"  alt="The Kite Map Logo" height="100" src="http://thekitemap.com/images/slide/add-beach.jpg">
+          </div>
+        </div>
+      </a>
+      
+      <a href="location.php">
+        <div class="icons3">
+          <div class="icon-slide-container">
+            <img class="slide-icon"  alt="The Kite Map Logo" height="100" src="http://thekitemap.com/images/slide/view-all.jpg">
+            </div>
+          </div>
+        </a>
+        
+        <a href="feedback.html">
+          <div class="icons4">
+            <div class="icon-slide-container">
+              <img class="slide-icon"  alt="The Kite Map Logo" height="100" src="http://thekitemap.com/images/slide/feedback.jpg">
+              </div>
+            </div>
+          </a>
+
+
+	</div>
+
+
+</section>
+
+<section>
+<div id="icon-wrapper">
+  <a href="map.html">
+    <div class="icons">
+      <div class="icon-slide-container">
+        <img class="slide-icon"  alt="The Kite Map Logo" height="100" src="http://thekitemap.com/images/slide/view-map.jpg">
+        </div>
+      </div>
+    </a>
+    
+    <a href="kite-map.php">
+      <div class="icons2">
+        <div class="icon-slide-container">
+          <img class="slide-icon"  alt="The Kite Map Logo" height="100" src="http://thekitemap.com/images/slide/add-beach.jpg">
+          </div>
+        </div>
+      </a>
+      
+      <a href="location.php">
+        <div class="icons3">
+          <div class="icon-slide-container">
+            <img class="slide-icon"  alt="The Kite Map Logo" height="100" src="http://thekitemap.com/images/slide/view-all.jpg">
+            </div>
+          </div>
+        </a>
+        
+        <a href="feedback.html">
+          <div class="icons4">
+            <div class="icon-slide-container">
+              <img class="slide-icon"  alt="The Kite Map Logo" height="100" src="http://thekitemap.com/images/slide/feedback.jpg">
+              </div>
+            </div>
+          </a>
+
+
+	</div>
+
+	
+</section>
+</body>
+</html>
---
+++ layouts/sidebox.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+$("#dashboardBody").slideToggle("fast"); // The Body of "Messages" is already opened in the design sample.
+
+  $("#dashboard").click(function(){
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
+    $(".tabBody").slideUp("fast");
+    $("#dashboardBody").slideToggle("fast");
+  });
+    $("#profile").click(function(){
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
+    $(".tabBody").slideUp("fast");
+    $("#profileBody").slideToggle("fast");
+  });
+  $("#messages").click(function(){
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
+    $(".tabBody").slideUp("fast");
+    $("#messagesBody").slideToggle("fast");
+  });
+  $("#settings").click(function(){
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
+    $(".tabBody").slideUp("fast");
+    $("#settingsBody").slideToggle("fast");
+  });
+  $("#city1").click(function(){
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
+    $(".tabBody").slideUp("fast");
+    $("#city1Body").slideToggle("fast");
+  });
+  $("#city2").click(function(){
+    $(".tab").removeClass("tabSelected");
+    $(".tab").addClass("tabNoSelected");
+    $(this).removeClass("tabNoSelected");
+    $(this).addClass("tabSelected");
+    $(".tabBody").slideUp("fast");
+    $("#city2Body").slideToggle("fast");
+  });
+});
